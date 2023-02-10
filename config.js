a794e2f02901dface7766f148e5648f4c08afb13
Andres Gomez <andresjr41@gmail.com>
2023-02-09 22:57:28 -0500

    config.js

---
+++ config.js
@@ -0,0 +1,50 @@
+const configs = {
+  api:{
+    port: process.env.PORT || 3000,
+    host: process.env.HOST || 'http://localhost:3000',
+    nodeEnv: process.env.NODE_ENV || 'development'
+  },
+  db:{
+    development:{
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'root',
+      database: 'chatDB2023',
+      define:{
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true
+      }
+    },
+    production:{
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'root',
+      database: 'chatDB2023',
+      define:{
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true
+      }
+    },
+    testing:{
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'root',
+      database: 'chatDB2023',
+      define:{
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true
+      }
+    }
+  }
+}
+
+module.exports = configs
